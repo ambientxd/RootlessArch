89f337ab3784568f6c3594fcf4a6aed3a2101c80
ambient <ambient@epicme.ga>
2022-09-30 16:58:46 +0700

    args

--- binwrappers/wrapper.sh
+++ binwrappers/wrapper.sh
@@ -2,6 +2,7 @@
 
 # Configurations for custom usage.
 InstallerFile="%INSTALLERFILE%"
+CustomArgs=""
 
 # Running installer.sh
-bash $InstallerFile --run "%COMMAND%" $@
+bash $InstallerFile --run "%COMMAND%" $CustomArgs$@
